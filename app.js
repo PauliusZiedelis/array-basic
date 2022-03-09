b03f59e0b7a5223c69d0427747b0c1066cebccc5
Paulius <p.ziedelis@gmail.com>
2022-03-09 23:50:49 +0200

    all 9 tasks

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ function rand(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-//task1 Sugeneruokite masyvą iš 30 elementų (indeksai nuo 0 iki 29), kurių reikšmės yra atsitiktiniai skaičiai nuo 5 iki 25.
+//task1 Sugeneruokite masyvą iš 30 elementų (indeksai nuo 0 iki 29), kurių reikšmės yra atsitiktiniai skaičiai 
+//nuo 5 iki 25.
 const array = [];
 for(let i = 0; i < 30; i++){
     array.push(rand(5, 25));
@@ -70,7 +71,8 @@ for(let i = 0; i < needAddElements; i++){
 }
 console.log(arrayNew);
 
-//f Iš masyvo elementų sukurkite du naujus masyvus. Vienas turi būti sudarytas iš neporinių indeksų reikšmių, o kitas iš porinių (pagal neporinį-porinį indeksą, ne reikšmę);
+//f Iš masyvo elementų sukurkite du naujus masyvus. Vienas turi būti sudarytas iš neporinių indeksų reikšmių, 
+//o kitas iš porinių (pagal neporinį-porinį indeksą, ne reikšmę);
 const arrayEven = [];
 const arrayOdd =[];
 arrayNew.forEach((value, index) => {
@@ -98,5 +100,134 @@ for(let i = 0; i < array.length; i++){
 }
 console.log('Pirmas array masyvo elemneto indeksas, kurio elemento reikšmė didesnė už 10: ', indexOfFirstValueBiggerThan10, ', elemento reikšmė: ', array[indexOfFirstValueBiggerThan10]);
 
-//task3 Sugeneruokite masyvą, kurio reikšmės atsitiktinės raidės A, B, C ir D, o ilgis 200. Suskaičiuokite kiek yra kiekvienos raidės.
+//task3 Sugeneruokite masyvą, kurio reikšmės atsitiktinės raidės A, B, C ir D, o ilgis 200. 
+//Suskaičiuokite kiek yra kiekvienos raidės.
+const arrayOfABCD = [];
+let counterA = 0;
+let counterB = 0;
+let counterC = 0;
+let counterD = 0;
+for(let i = 0; i < 200; i++){
+    const random = rand(0, 3);
+    if(random === 0){
+        arrayOfABCD.push('A');
+        counterA++;
+    }else if(random === 1){
+        arrayOfABCD.push('B');
+        counterB++;
+    }else if(random === 2){
+        arrayOfABCD.push('C');
+        counterC++;
+    }else if(random === 3){
+        arrayOfABCD.push('D');
+        counterD++;
+    }
+}
+console.log('Masyve: ', arrayOfABCD, ` yra A - ${counterA}, yra B - ${counterB}, yra C - ${counterC}, yra D - ${counterD},`);
 
+//task4 Sugeneruokite 3 masyvus pagal 3 uždavinio sąlygą. Sudėkite masyvus, sudėdami atitinkamas reikšmes. 
+// Paskaičiuokite kiek unikalių (po vieną, nesikartojančių) reikšmių ir kiek unikalių kombinacijų gavote.
+function arrayOfABCDMaker (arrayLength){
+    const arrayOfABCD = [];
+    for(let i = 0; i < arrayLength; i++){
+        const random = rand(0, 3);
+        if(random === 0){
+            arrayOfABCD.push('A');
+        }else if(random === 1){
+            arrayOfABCD.push('B');
+        }else if(random === 2){
+            arrayOfABCD.push('C');
+        }else if(random === 3){
+            arrayOfABCD.push('D');
+        }
+    }
+    return arrayOfABCD;
+}
+const arrayOfABCD1 = arrayOfABCDMaker(200);
+const arrayOfABCD2 = arrayOfABCDMaker(200);
+const arrayOfABCD3 = arrayOfABCDMaker(200);
+const sumOfArrayABCD = [];
+const uniqueCombination = {};
+const uniqueValue = [];
+arrayOfABCD1.forEach((value, index) => {
+    const string = arrayOfABCD1[index]+arrayOfABCD2[index]+arrayOfABCD3[index];
+    sumOfArrayABCD.push(string);
+    uniqueCombination.hasOwnProperty(string) ? uniqueCombination[string]++ : uniqueCombination[string] = 1;
+})
+Object.keys(uniqueCombination).forEach(key => {
+    if(uniqueCombination[key] === 1){
+        uniqueValue.push(key);
+    }
+  });
+console.log(sumOfArrayABCD);
+console.log('Unikalių reikšmių yra: ', uniqueValue.length, ' Unikalios reikšmės yra ', uniqueValue);
+console.log('Unikalių kombinacijų ',Object.keys(uniqueCombination).length);
+
+//task5 Sugeneruokite du masyvus, kurių reikšmės yra atsitiktiniai skaičiai nuo 100 iki 999. Masyvų ilgiai 100. 
+//Masyvų reikšmės turi būti unikalios savo masyve (t.y. neturi kartotis).
+/**
+ * 
+ * @param {number} length length of array you want generate
+ * @param {number} minNumber minimum array value
+ * @param {number} maxNumber max array value
+ * @returns {[]} array of radom not repeated numbers
+ */
+function arrayOfRandomNumbersThatNotReapeats (length, minNumber, maxNumber){
+    if((maxNumber - minNumber) < length){
+        console.error('Array length is to big')
+        return [];
+    }
+    const array = [];
+    while(array.length < length){
+        const randomNumber = rand(minNumber, maxNumber);
+        if(!array.includes(randomNumber)){
+            array.push(randomNumber);
+        }
+    }
+    return array;
+}
+const arrayofRandomNumbers1 = arrayOfRandomNumbersThatNotReapeats(100, 100, 999);
+const arrayofRandomNumbers2 = arrayOfRandomNumbersThatNotReapeats(100, 100, 999);
+console.log(arrayofRandomNumbers1);
+console.log(arrayofRandomNumbers2);
+
+//task6 Sugeneruokite masyvą, kuris būtų sudarytas iš reikšmių, kurios yra pirmame 6 uždavinio masyve, 
+// bet nėra antrame 6 uždavinio masyve.
+const differentValuesArray = [];
+arrayofRandomNumbers1.forEach(value => {
+    if(!arrayofRandomNumbers2.includes(value)){
+        differentValuesArray.push(value);
+    }
+})
+console.log(differentValuesArray);
+
+//Task7 Sugeneruokite masyvą iš elementų, kurie kartojasi abiejuose 6 uždavinio masyvuose.
+const sameValuesArray = [];
+arrayofRandomNumbers1.forEach(value => {
+    if(arrayofRandomNumbers2.includes(value)){
+        sameValuesArray.push(value);
+    }
+})
+console.log(sameValuesArray);
+
+//Task8 Sugeneruokite masyvą, kurio indeksus sudarytų pirmo 6 uždavinio masyvo reikšmės, 
+//o jo reikšmės iš būtų antrojo masyvo.
+const strangeArray = [];
+arrayofRandomNumbers1.forEach((value, index) => {
+    strangeArray[value] = arrayofRandomNumbers2[index];
+});
+console.log(strangeArray);
+
+//Task9 Sugeneruokite 10 skaičių masyvą pagal taisyklę: Du pirmi skaičiai- atsitiktiniai nuo 5 iki 25. Trečias, 
+//pirmo ir antro suma. Ketvirtas- antro ir trečio suma. Penktas trečio ir ketvirto suma ir t.t.
+function kindaFibonacciArray (length){
+    const array = [];
+    array.push(rand(5, 25));
+    array.push(rand(5, 25));
+    for(let i = 0; array.length < length; i++){
+        array.push(array[i] + array[i+1]);
+    }
+    return array;
+}
+const task9Array = kindaFibonacciArray(10);
+console.log(task9Array);
